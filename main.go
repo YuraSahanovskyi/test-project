0e9dfd06ebe85d7abf5002c2a93a873173f77ae0
YuraSahanovskyi <yuriisahanovskyi@gmail.com>
2023-08-21 13:07:32 +0300

    Logs for server start

--- main.go
+++ main.go
@@ -6,11 +6,11 @@ import (
 )
 
 func main() {
+	log.Println("servers started at :8080")
 	err := http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, World!"))
 	}))
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println("servers started at :8080")
 }
