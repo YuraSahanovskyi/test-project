913b8a566ad2d4c576d3b2d55ec2f02ca621a82c
Yura Sahanovskyi <yuriisahanovskyi@gmail.com>
2024-03-29 16:05:15 +0200

    Comment added

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+	//testing git
 	log.Println("servers started at :8080")
 	err := http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, World!"))
